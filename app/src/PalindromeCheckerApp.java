c4aafb24008c2894319df2f89fe86db71a719765
AbhinavTiwari7 <abnv.slattt@gmail.com>
2026-02-23 15:54:25 +0530

    Abhinav Updated UC2 Logic

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -2,8 +2,19 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management system "
-                + "version : 1.0 "
-                + "System initialized successfully.");
+        System.out.println("UseCase 2 - Hardcoded Palindrome Check");
+
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Palindrome ✅");
+        } else {
+            System.out.println("Not Palindrome ❌");
+        }
     }
 }
